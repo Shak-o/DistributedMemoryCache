01983e40f1b8567606740c77a6a8296628bd659a
Shak-o <shalva.turashvili.1@btu.edu.ge>
2023-10-07 18:21:56 +0400

    update

--- DistributedMemm.Lib/Implementation/DistributedMemmImpl.cs
+++ DistributedMemm.Lib/Implementation/DistributedMemmImpl.cs
@@ -1,11 +1,12 @@
 using System.Collections.Concurrent;
+using DistributedMemm.Lib.Exceptions;
 using DistributedMemm.Lib.Interfaces;
 
 namespace DistributedMemm.Lib.Implementation;
 
 public class DistributedMemmImpl : IDistributedMemm
 {
-    private ConcurrentDictionary<string, object> _cache = new ();
+    private ConcurrentDictionary<string, string> _cache = new ();
     
     public void Upsert(string key, string value)
     {
@@ -27,9 +28,20 @@ public class DistributedMemmImpl : IDistributedMemm
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Update existing record in cache
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
     public void UpdateString(string key, string value)
     {
-        throw new NotImplementedException();
+        var existing = _cache[key];
+        if (string.IsNullOrEmpty(existing))
+        {
+            throw new ObjectNotFoundException(nameof(key));
+        }
+
+        _cache[key] = value;
     }
 
     public Task UpdateStringAsync(string key, string value)
