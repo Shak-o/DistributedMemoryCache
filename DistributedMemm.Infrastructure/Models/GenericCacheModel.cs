10b9e8907750b595f1b7e612bcdbe71e27fd0580
Kokooch <kkokochinchaladze@gmail.com>
2023-10-08 01:38:40 +0400

    memory management??

--- DistributedMemm.Infrastructure/Models/GenericCacheModel.cs
+++ DistributedMemm.Infrastructure/Models/GenericCacheModel.cs
@@ -4,4 +4,15 @@ public class GenericCacheModel
 {
     public int Version { get; set; }
     public object Value { get; set; } = null!;
+
+    public PriorityLevel Priority { get; set; }
+}
+
+public enum PriorityLevel
+{
+    Indeterminate,
+    Low,
+    Medium,
+    High,
+    Critical
 }
